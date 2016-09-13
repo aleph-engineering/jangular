3ba24de747c75dc82ae1f306457eb3f0cd667ec3
Michel Sánchez Montells <montells@aleph.engineering>
2016-09-13 13:33:03 -0400

    angular as explicit dependencies (#1)
    merging dependency fixing

--- karma.conf.js
+++ karma.conf.js
@@ -3,7 +3,7 @@ module.exports = function (config) {
     config.set({
         basePath: './',
         files: [
-            'node_modules/angular-ui-router/node_modules/angular/angular.js',
+            'node_modules/angular/angular.js',
             'node_modules/angular-ui-router/release/angular-ui-router.js',
             'node_modules/angular-mocks/angular-mocks.js',
 
